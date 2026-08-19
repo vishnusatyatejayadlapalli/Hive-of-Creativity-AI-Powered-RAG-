498912ed3a34d3b638bb2bd5fb86e2f3caae5ef1
vishnusatyatejayadlapalli <vishnusatyatejayadlapalli@users.noreply.github.com>
2026-08-19 19:48:54 +0530

    homejsx update

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-4xl font-bold">Hive of Creativity</h1>
+      <p>Turn your ideas and materials into something creative.</p>
+    </div>
+  )
+}
+
+export default Home
